a75fefa7669b4fc81e21e83214f3f83050aeba5b
ivoronina <doorinclouds@mail.ru>
2022-11-10 21:33:42 +0300

    Commit

--- Telebot/main.py
+++ Telebot/main.py
@@ -2,7 +2,6 @@ import random
 import time
 
 import markup as markup
-from aiogram.types import reply_keyboard, ReplyKeyboardMarkup, update
 
 from config import tg_bot_token
 from aiogram import Bot, types
@@ -12,15 +11,13 @@ from aiogram.utils import executor
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
 
-def start(bot, update):
-    update.message.reply_text('Hello, I`m smart password generator. I can help you to create safety password.',reply_markup=markup)
 
 reply_keyboard = [["/start"]]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     await message.answer("Hello, I`m smart password generator. I can help you to create safety password.")
-    time.sleep(3)
+    time.sleep(2)
     await message.answer("How many symbols of password do you want to create?")
 
 @dp.message_handler()
@@ -41,7 +38,7 @@ async def get_password(message: types.Message):
             time.sleep(1)
             await message.answer(f"Your password: {password}")
             time.sleep(2)
-            await message.answer("Would you like to create one more password? :)")
+            await message.answer("One more password? Just enter the number :)")
     except Exception as ex2:
         print(ex2)
         await message.answer("Enter the number of symbols from 5 to 15")
